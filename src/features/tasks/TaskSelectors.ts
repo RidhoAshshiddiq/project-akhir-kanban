92f11ddd2c9c4671e86e1396b8a20d25482e96d1
Ridho Ashshiddiq <lusiaanda46@gmail.com>
2023-10-21 00:10:55 +0700

    Develop the layouts of TaskList and TaskProgress pages

--- src/features/tasks/TaskSelectors.ts
+++ src/features/tasks/TaskSelectors.ts
@@ -2,12 +2,30 @@ import { selector } from 'recoil'
 import { tasksState } from './TaskAtoms'
 import type { Task } from '../../types'
 
-export const uncompletedTasksSelector = selector<Task[]>({
-  key: 'uncompleted_tasks',
+export const notStartedTasksSelector = selector<Task[]>({
+  key: 'notStarted_tasks',
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
-      return task.progressOrder !== 4
-    })
+      return task.progressOrder === 1;
+    });
+  },
+})
+
+export const inProgressTasksSelector = selector<Task[]>({
+  key: 'inProgress_tasks',
+  get: ({ get }) => {
+    return get(tasksState).filter((task) => {
+      return task.progressOrder === 2;
+    });
+  },
+})
+
+export const waitingTasksSelector = selector<Task[]>({
+  key: 'waiting_tasks',
+  get: ({ get }) => {
+    return get(tasksState).filter((task) => {
+      return task.progressOrder === 3;
+    });
   },
 })
 
@@ -15,7 +33,7 @@ export const completedTasksSelector = selector<Task[]>({
   key: 'completed_tasks',
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
-      return task.progressOrder === 4
-    })
+      return task.progressOrder === 4;
+    });
   },
 })
