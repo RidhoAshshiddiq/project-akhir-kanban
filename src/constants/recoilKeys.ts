58186aaf7148e21d4b4fcf81f8929a888eb5fb6a
Ridho Ashshiddiq <lusiaanda46@gmail.com>
2023-10-21 00:41:14 +0700

    Refactor raw data

---
+++ src/constants/recoilKeys.ts
@@ -0,0 +1,10 @@
+export const AtomKeys = {
+    TASKS: 'tasks',
+  }
+  
+  export const SelectorKeys = {
+    NOT_STARTED_TASKS: 'notStartedTasks',
+    IN_PROGRESS_TASKS: 'inProgressTasks',
+    WAITING_TASKS: 'waitingTasks',
+    COMPLETED_TASKS: 'completedTasks',
+  }
