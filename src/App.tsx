930057f4da892bce9821da04b32965f3ea902930
Ridho Ashshiddiq <lusiaanda46@gmail.com>
2023-11-11 16:04:35 +0700

    Menambahkan konfigurasi untuk Halaman Github

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,14 @@ import TaskList from './features/components/TaskLists/TaskList';
 import TaskProgress from './features//components/TaskProgress/TaskProgress';
 
 
-// const Home: React.FC = () => <h1>Home</h1>;
-// const TaskList: React.FC = () => <h1>Task List</h1>;
-// const TaskProgress: React.FC = () => <h1>Task Progress</h1>;
 
 const App: React.FC = () => {
   return (
     <RecoilRoot> {/* Ditambahkan */}
-      <Router>
+       <Router basename="/project-akhir-kanban">
         <div style={{ display: 'flex' }}>
           <SideMenu />
-          <div style={{ marginLeft: '-20px', padding: '20px' , marginTop: '-40px', }}>
+          <div style={{ marginLeft: '-20px', padding: '20px', marginTop: '-40px' }}>
             <Routes>
               <Route path="/" element={<TaskSummary />} />
               <Route path="/task-list" element={<TaskList />} />
